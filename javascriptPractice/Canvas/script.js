69437334e4d1d0abbb80c7832cda6fd753899b97
JlordS32 <jlordsty128@gmail.com>
2022-09-10 15:00:18 +1000

    #

--- javascriptPractice/Canvas/script.js
+++ javascriptPractice/Canvas/script.js
@@ -1,39 +1,39 @@
 window.addEventListener("load", () => {
-const canvas = document.querySelector("#canvas");
-const context = canvas.getContext("2d");
+    const canvas = document.querySelector("#canvas");
+    const context = canvas.getContext("2d");
 
-canvas.height = innerWidth;
-canvas.width = innerWidth;
+    canvas.height = innerWidth;
+    canvas.width = innerWidth;
 
-let painting= false;
+    let painting= false;
 
-function startPosition(){
-    painting = true;
-    draw(e);
-};
+    function startPosition(){
+        painting = true;
+        draw(e);
+    };
 
-function endPosition(){
-    painting = false;
-    context.beginPath();
-};
+    function endPosition(){
+        painting = false;
+        context.beginPath();
+    };
 
-function draw(e){
-    if(!painting) return;
+    function draw(e){
+        if(!painting) return;
 
-    context.lineWidth = 10;
-    context.lineCap = "round";
-    context.strokeStyle = "blue";
+        context.lineWidth = 10;
+        context.lineCap = "round";
+        context.strokeStyle = "blue";
 
-    context.lineTo(e.clientX, e.clientY);
-    context.stroke();
-    context.beginPath();
-    context.moveTo(e.clientX, e.clientY);
-};
+        context.lineTo(e.clientX, e.clientY);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(e.clientX, e.clientY);
+    };
 
-canvas.addEventListener('mousedown', startPosition);
-canvas.addEventListener('mouseup', endPosition);
-canvas.addEventListener('mousemove', draw);
-});
+    canvas.addEventListener('mousedown', startPosition);
+    canvas.addEventListener('mouseup', endPosition);
+    canvas.addEventListener('mousemove', draw);
+});\watch
 
 /*
 context2.strokeStyle = "green";
